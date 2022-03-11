7a0a12da32c571dda7ad43d4503f4da5b38ce630
Björn Dahlgren <bjorn@dahlgren.io>
2022-03-11 03:26:29 +0100

    Use arma3sync-lib instead of java application for writing events

--- config.example.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  arma3sync: '/path/to/arma3sync/data/directory',
-  java: '/path/to/java/binary',
-  path: '/path/to/arma3sync/output/directory',
-  repository: 'repository_name'
-}
--- events.js
+++ events.js
@@ -1,8 +1,7 @@
-var path = require('path')
-var request = require('request')
-var spawn = require('child_process').spawn
+require('dotenv').config()
 
-var config = require('./config')
+var arma3syncLib = require('arma3sync-lib')
+var request = require('request')
 
 function fetchOperations (cb) {
   request.get({url: 'http://anrop.se/api/operations', json: true}, function (err, res) {
@@ -53,41 +52,7 @@ function createTemplateEvents (templates) {
 }
 
 function writeEvents (events) {
-  var java = [
-    '-jar',
-    path.resolve(__dirname, 'arma3sync.jar')
-  ]
-
-  var args = [
-    'setevents',
-    '--repository',
-    config.repository,
-    '--path',
-    config.path,
-    '--json',
-    '--events-json',
-    JSON.stringify(events)
-  ]
-
-  var childProcess = spawn(config.java, java.concat(args), {
-    cwd: config.arma3sync, env: process.env
-  })
-
-  childProcess.stdout.on('data', function (data) {
-    console.log(data.toString())
-  })
-
-  childProcess.stderr.on('data', function (data) {
-    console.log(data.toString())
-  })
-
-  childProcess.on('close', function (code) {
-    process.exit(code)
-  })
-
-  childProcess.on('error', function (code) {
-    console.log(code)
-  })
+  arma3syncLib.a3sDirectory.setEvents({ list: events })
 }
 
 fetchOperations(function (err, operations) {
